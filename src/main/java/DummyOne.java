3a3f29b97772bd7d2d06be79167b874d5a01bb02
zaksignu <55884879+zaksignu@users.noreply.github.com>
2022-03-30 00:08:58 +0300

    minor update

---
+++ src/main/java/DummyOne.java
@@ -0,0 +1,9 @@
+import lombok.Data;
+
+@Data
+public class DummyOne {
+    private final String firstName;
+    private final String lastName;
+    private final String phone;
+    private final String date;
+}
